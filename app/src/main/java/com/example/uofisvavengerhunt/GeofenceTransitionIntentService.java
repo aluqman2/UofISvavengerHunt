9d8291096a9b94b6868759ba407f15e90b6b5848
aluqman2 <aluqman2@illinois.edu>
2019-04-23 13:43:39 -0500

    dumpster fire

---
+++ app/src/main/java/com/example/uofisvavengerhunt/GeofenceTransitionIntentService.java
@@ -0,0 +1,27 @@
+package com.example.uofisvavengerhunt;
+
+import android.app.IntentService;
+import android.content.Intent;
+import android.util.Log;
+
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingEvent;
+
+import static android.content.ContentValues.TAG;
+
+public class GeofenceTransitionIntentService extends IntentService {
+
+    public GeofenceTransitionIntentService(String name) {
+        super(name);
+    }
+
+    protected void onHandleIntent(Intent intent) {
+        GeofencingEvent event = GeofencingEvent.fromIntent(intent);
+
+        int geofenceTransition = event.getGeofenceTransition();
+
+        if (geofenceTransition == Geofence.GEOFENCE_TRANSITION_DWELL) {
+            Log.d(TAG, "Holy fuck");
+        }
+    }
+}
--- app/src/main/java/com/example/uofisvavengerhunt/MapsActivity.java
+++ app/src/main/java/com/example/uofisvavengerhunt/MapsActivity.java
@@ -1,45 +1,88 @@
 package com.example.uofisvavengerhunt;
 
 import android.Manifest;
+import android.app.PendingIntent;
+import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
+import android.location.LocationManager;
+import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 import android.support.v4.content.ContextCompat;
 import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
+import com.google.android.gms.common.api.GoogleApiClient;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.Geofence;
+import com.google.android.gms.location.GeofencingClient;
+import com.google.android.gms.location.GeofencingRequest;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.location.LocationSettingsRequest;
+import com.google.android.gms.location.LocationSettingsResponse;
+import com.google.android.gms.location.SettingsClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+
+import java.util.ArrayList;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
     private GoogleMap mMap;
     private Location lastLocation;
     private static final String TAG = "MainActivity";
+    private FusedLocationProviderClient fusedLocationClient;
+
+    private LocationRequest locationRequest;
+    private GeofencingClient geofencingClient;
+
+    private PendingIntent geofencePendingIntent;
+
+    private ArrayList<Geofence> geofences = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
+        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this);
         checkPermissions();
+        createLocationRequest();
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
+
+        geofencingClient = LocationServices.getGeofencingClient(this);
 
         final Button jeff = findViewById(R.id.button);
         jeff.setOnClickListener(v -> {
             Log.d(TAG, "Updating Location");
             updateLocation();
         });
-    }
 
+        geofences.add(new Geofence.Builder()
+                .setRequestId("Siebel")
+                .setExpirationDuration(1000000000)
+                .setTransitionTypes(Geofence.GEOFENCE_TRANSITION_DWELL)
+                .setCircularRegion(40, -88, 50)
+                .setLoiteringDelay(1000)
+                .build());
+
+        geofencingClient.addGeofences(getGeofencingRequest(), getGeofencePendingIntent());
+    }
 
     /**
      * Manipulates the map once available.
@@ -53,15 +96,28 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     @Override
     public void onMapReady(GoogleMap googleMap) {
         mMap = googleMap;
-
+        mMap.setMyLocationEnabled(true);
         // Add a marker in Sydney and move the camera
+
         LatLng sydney = new LatLng(-34, 151);
         mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
     }
 
     public void updateLocation() {
-        // Grab current location, update via button.
+        Task locationResult = fusedLocationClient.getLastLocation();
+        locationResult.addOnCompleteListener(this, new OnCompleteListener() {
+            @Override
+            public void onComplete(@NonNull Task task) {
+                if (task.isSuccessful()) {
+                    lastLocation = (Location) task.getResult();
+                    mMap.moveCamera(CameraUpdateFactory.newLatLng(
+                            new LatLng(lastLocation.getLatitude(),
+                                    lastLocation.getLongitude())
+                    ));
+                }
+            }
+        });
     }
 
     private void checkPermissions() {
@@ -71,5 +127,26 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
     }
 
+    protected void createLocationRequest() {
+        locationRequest = LocationRequest.create();
+        locationRequest.setInterval(1000);
+        locationRequest.setFastestInterval(500);
+        locationRequest.setPriority(LocationRequest.PRIORITY_HIGH_ACCURACY);
+    }
 
+    private GeofencingRequest getGeofencingRequest() {
+        GeofencingRequest.Builder request = new GeofencingRequest.Builder();
+        request.setInitialTrigger(GeofencingRequest.INITIAL_TRIGGER_DWELL);
+        request.addGeofences(geofences);
+        return request.build();
+    }
+
+    private PendingIntent getGeofencePendingIntent() {
+        if (geofencePendingIntent != null) {
+            return geofencePendingIntent;
+        }
+        Intent intent = new Intent(this, GeofenceTransitionIntentService.class);
+        geofencePendingIntent = PendingIntent.getService(this, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT);
+        return geofencePendingIntent;
+    }
 }
